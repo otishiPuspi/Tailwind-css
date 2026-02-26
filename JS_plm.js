75f12e828edf8902f04494ce11126f8c5778660d
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-02-26 14:16:53 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -19,3 +19,12 @@
 ‎    fact *= i;
 ‎}
 ‎console.log("Factorial is: " + fact);
+
+‎// 4. Write a JavaScript program to check if a number is even or odd.
+‎
+‎let number = 7;
+‎if (number % 2 === 0) {
+‎    console.log("Even");
+‎} else {
+‎    console.log("Odd");
+‎}
