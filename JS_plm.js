9b31c72c3c233570fd5e1db8e05ba66c7cd78ae2
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-03-01 06:07:44 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1,15 +1,4 @@
-‎// 6. Write a JavaScript program to check if a number is positive, negative or zero.
-‎
-‎let n = -5;
-‎
-‎if (n > 0) {
-‎    console.log("Positive");
-‎} else if (n < 0) {
-‎    console.log("Negative");
-‎} else {
-‎    console.log("Zero");
-‎}
-‎// 1. Write a JavaScript program to print numbers from 1 to 10.
+‎‎// 1. Write a JavaScript program to print numbers from 1 to 10.
 ‎
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
@@ -48,4 +37,24 @@
 ‎    console.log("Largest is: " + a);
 ‎} else {
 ‎    console.log("Largest is: " + b);
+‎}
+
+// 6. Write a JavaScript program to check if a number is positive, negative or zero.
+‎
+‎let n = -5;
+‎
+‎if (n > 0) {
+‎    console.log("Positive");
+‎} else if (n < 0) {
+‎    console.log("Negative");
+‎} else {
+‎    console.log("Zero");
+‎}
+
+‎// 7. Write a JavaScript program to print multiplication table of 5.
+‎
+‎let table = 5;
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(table + " x " + i + " = " + (table * i));
 ‎}
