1a90ec3c06791a0307cb78edb10bfe7c79e368de
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-05-05 09:00:09 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -801,3 +801,17 @@
 ‎}
 ‎
 ‎console.log("Missing:", total - sum2);
+
+
+‎// 72. Write a JavaScript program to remove negative numbers from array.
+‎
+‎let arr4 = [1,-2,3,-4,5];
+‎let positive = [];
+‎
+‎for (let i = 0; i < arr4.length; i++) {
+‎    if (arr4[i] >= 0) {
+‎        positive.push(arr4[i]);
+‎    }
+‎}
+‎
+‎console.log(positive);
