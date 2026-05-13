df25b931273307a893314ae22c6e31f173816fbf
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-05-13 17:40:51 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -886,3 +886,10 @@ for (let i = 1; i <= number; i++) {
 
 console.log("Factorial of " + number + " is: i" + factorial);
 
+‎// 79. Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
