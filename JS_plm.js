3145cb3cf3bdd8093e82f741d1eae75504df34f8
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-05-20 20:18:13 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -959,3 +959,16 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎    fact *= i;
 ‎}
 ‎console.log("Factorial is: " + fact);
+
+
+// 86. Write a JavaScript program to find the factorial of a number.
+
+let number = 5;
+let factorial = 1;
+
+for (let i = 1; i <= number; i++) {
+    factorial = factorial * i;
+}
+
+console.log("Factorial of " + number + " is: i" + factorial);
+
