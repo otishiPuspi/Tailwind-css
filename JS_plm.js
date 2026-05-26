97bd254bc819a76a7246c44510823b5caf536731
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-05-26 19:57:03 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1031,3 +1031,9 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎
 ‎console.log("Array sum: " + total);
+
+‎// 92.Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
