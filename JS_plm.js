ba8f0d4f84a0bfbfb8fc55a7b4525c97086d5174
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-05-29 19:34:01 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1069,3 +1069,10 @@ if (isPrime) {
 } else {
     console.log(number + " is not a Prime number");
 }
+
+
+‎// 95. Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
