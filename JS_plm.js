b7fb7c0f1cbe1896fe5d9eec099332f4a6a5d300
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-06-09 09:39:38 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1185,3 +1185,13 @@ let arr = [1,2,3,4,5]
 ‎}
 ‎console.log("Sum is: " + sum);
 ‎
+‎// 106. Write a JavaScript program to find the factorial of a number.
+‎
+‎let num = 5;
+‎let fact = 1;
+‎for (let i = 1; i <= num; i++) {
+‎    fact *= i;
+‎}
+‎console.log("Factorial is: " + fact);
+
+
