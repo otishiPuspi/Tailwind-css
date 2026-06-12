52c0ba828e72c4f30defe17c6b0b89bbcecdefd3
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-06-12 19:11:21 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1212,3 +1212,13 @@ let arr = [1,2,3,4,5]
 ‎} else {
 ‎    console.log("Odd");
 ‎}
+
+‎// 108. Write a JavaScript program to find the sum of first 10 natural numbers.
+‎
+‎let sum = 0;
+‎for (let i = 1; i <= 10; i++) {
+‎    sum += i;
+‎}
+‎console.log("Sum is: " + sum);
+‎
+‎
