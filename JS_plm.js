3dc83a9f71baa2cdc90828e5d5b9c6521bb67c37
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-06-19 18:12:12 +0600

    Implement largest number finder in JS
    Added a program to find the largest of two numbers.

--- JS_plm.js
+++ JS_plm.js
@@ -1291,3 +1291,15 @@ let arr = [1,2,3,4,5]
 ‎    f1 = f2;
 ‎    f2 = next;
 ‎}
+
+
+‎// Write a JavaScript program to find the largest of two numbers.
+‎
+‎let a = 10;
+‎let b = 20;
+‎Z
+‎if (a > b) {
+‎    console.log("Largest is: " + a);
+‎} else {
+‎    console.log("Largest is: " + b);
+‎}
