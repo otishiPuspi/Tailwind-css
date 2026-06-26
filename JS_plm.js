b08c8374db0ee425f2b3c755aae16b52f99ecd92
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-06-26 22:07:22 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1370,3 +1370,10 @@ let arr = [1,2,3,4,5]
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+
+‎// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
