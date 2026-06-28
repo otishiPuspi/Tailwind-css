6e3b74783ba0121654b4f92ce9cfb99169626dfb
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-06-28 19:32:19 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1390,3 +1390,14 @@ let arr = [1,2,3,4,5]
 ‎} else {
 ‎    console.log("Zero");
 ‎}
+
+//  Write a JavaScript program to reverse a string.
+‎
+‎let str = "hello";
+‎let reversed = "";
+‎
+‎for (let i = str.length - 1; i >= 0; i--) {
+‎    reversed += str[i];
+‎}
+‎
+‎console.log("Reversed: " + reversed);
