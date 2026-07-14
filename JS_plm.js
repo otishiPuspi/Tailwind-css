01f57649f9dc5497446a45d857d1b0dcc6f25c87
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-07-14 09:56:01 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1549,3 +1549,9 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎} else {
 ‎    console.log("Largest is: " + b);
 ‎}
+
+‎// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
