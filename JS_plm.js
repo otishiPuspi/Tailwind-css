37244432730055faf448432e56a75ed2cf40d334
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-07-22 20:11:01 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1598,3 +1598,15 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎for (let i = 1; i <= 10; i++) {
 ‎    console.log(i);
 ‎}
+
+‎//  Write a JavaScript program to check if a number is positive, negative or zero.
+‎
+‎let n = -5;
+‎
+‎if (n > 0) {
+‎    console.log("Positive");
+‎} else if (n < 0) {
+‎    console.log("Negative");
+‎} else {
+‎    console.log("Zero");
+‎}
