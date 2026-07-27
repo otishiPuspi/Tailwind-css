838eec0263f50c16183eb150953456b70aec84a5
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-07-27 21:28:23 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -1639,3 +1639,15 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎}
 ‎
 ‎console.log("Reversed: " + reversed);
+
+
+// Write a JavaScript program to find the factorial of a number.
+
+let number = 5;
+let factorial = 1;
+
+for (let i = 1; i <= number; i++) {
+    factorial = factorial * i;
+}
+
+console.log("Factorial of " + number + " is: i" + factorial);
