9504eee3e287d1741ce81f84f0689761883257be
Otishy Islam Puspi <otishy52.ist@gmail.com>
2026-07-30 22:43:23 +0600

    Update JS_plm.js

--- JS_plm.js
+++ JS_plm.js
@@ -153,6 +153,13 @@ console.log("Factorial of " + number + " is: i" + factorial);
 ‎
 ‎console.log("Array sum: " + total);
 
+
+// Write a JavaScript program to print numbers from 1 to 10.
+‎
+‎for (let i = 1; i <= 10; i++) {
+‎    console.log(i);
+‎}
+
 ‎// 14. Write a JavaScript program to swap two numbers.
 ‎
 ‎let x = 5;
